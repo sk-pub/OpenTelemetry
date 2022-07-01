4737347446206a221c9bde69933e106b5a505cb5
Sergei Koshel <unicgm@gmail.com>
2022-07-01 23:37:26 +0200

    Add tracing with OpenTelemetry

--- Api1/Program.cs
+++ Api1/Program.cs
@@ -1,7 +1,32 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddHttpClient();
+
+// Telemetry
+// Define some important constants and the activity source
+var serviceName = "Api1";
+var serviceVersion = "1.0.0";
+var serviceInstanceId = Environment.MachineName;
+
+builder.Services.AddOpenTelemetryTracing(b =>
+{
+    b
+    .AddConsoleExporter()
+    .AddJaegerExporter(b => b.Endpoint = new Uri("http://localhost:6831"))
+    .AddSource(serviceName)
+    .SetResourceBuilder(
+        ResourceBuilder.CreateDefault()
+            .AddService(serviceName: serviceName, serviceVersion: serviceVersion, serviceInstanceId: serviceInstanceId))
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation(b => b.RecordException = true);
+});
+
+// Logging
+builder.Logging.ClearProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
