e30c16974a125da51606019e3df21fa9ddc38b4f
Sergei Koshel <unicgm@gmail.com>
2022-07-01 23:50:03 +0200

    Add OpenTelemetry to Api2

--- Api2/Program.cs
+++ Api2/Program.cs
@@ -1,6 +1,29 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Telemetry
+// Define some important constants and the activity source
+var serviceName = "Api2";
+var serviceVersion = "1.0.0";
+var serviceInstanceId = Environment.MachineName;
+
+builder.Services.AddOpenTelemetryTracing(b =>
+{
+    b
+    .AddConsoleExporter()
+    .AddJaegerExporter(b => b.Endpoint = new Uri("http://localhost:6831"))
+    .AddSource(serviceName)
+    .SetResourceBuilder(
+        ResourceBuilder.CreateDefault()
+            .AddService(serviceName: serviceName, serviceVersion: serviceVersion, serviceInstanceId: serviceInstanceId))
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation(b => b.RecordException = true);
+});
+
+// Logging
+builder.Logging.ClearProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
